aae368f91d994de2cba217fade933e4d185186fc
AnnaKRP <annakarpiuk19.1994@gmail.com>
2025-02-07 20:41:58 +0000

    adding contact functionality

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Burger menu toggle functionality
     const burgerMenu = document.getElementById('burger-menu');
     const menu = document.getElementById('menu');
     const menuLinks = menu ? menu.querySelectorAll('a') : [];
 
-    // burger menu
     if (burgerMenu && menu) {
         burgerMenu.addEventListener('click', () => {
             console.log('Burger menu clicked');
@@ -13,12 +13,35 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    // close the menu when a link is clicked
+    // Close the menu when a link is clicked
     menuLinks.forEach(link => {
         link.addEventListener('click', () => {
             menu.classList.remove('active');
             burgerMenu.classList.remove('open');
             document.documentElement.classList.remove('no-scroll');
+        });
+    });
+
+    // initialize EmailJS
+    emailjs.init("OTV5OwPgyF8gW7LPd");
+
+    // handle form submission for sending email
+    const form = document.getElementById('contactForm');
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        emailjs.send(
+            "service_s2kellp",
+            "template_tfz8s7f",
+            {
+                name: document.getElementById("name").value,
+                email: document.getElementById("email").value,
+                message: document.getElementById("message").value
+            }
+        ).then(function(response) {
+            alert("Email sent successfully!");
+        }, function(error) {
+            alert("The service is temporarily unavailable. Please try again later.");
         });
     });
 });
